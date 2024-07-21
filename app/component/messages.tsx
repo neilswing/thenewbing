d02f3458c8159699a5129f88a8f4615848ef3c22
Neil Swing <njeswing23@bpsu.edu.ph>
2024-07-21 21:39:27 +0800

    first

--- app/component/messages.tsx
+++ app/component/messages.tsx
@@ -17,6 +17,7 @@ const Messages = ({ messages, isLoading }: Props) => {
       {messages.map((m, index) => {
         return (
           <div
+            key={index}
             className={`p-4 shadow-md rounded-md ml-10 relative ${
               m.role === "user" ? "bg-stone-300" : ""
             }`}
